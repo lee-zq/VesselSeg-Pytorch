fix memory overflow

train.py
@@ -1,89 +1,17 @@
 import torch.backends.cudnn as cudnn
 import torch.optim as optim
-from tqdm import tqdm
-import random,sys,time
+import sys,time
 from os.path import join
 import torch
-from lib.extract_patches import get_data_train
 from lib.losses.loss import *
-from lib.visualize import group_images, save_img
 from lib.common import *
-from lib.dataset import TrainDataset,TestDataset
-from torch.utils.data import DataLoader
 from config import parse_args
 from lib.logger import Logger, Print_Logger
-from collections import OrderedDict
-from lib.metrics import Evaluate
 import models
 from test import Test
 
+from function import get_dataloader, train, val, get_dataloaderV2
 
-#  Load the data and extract patches
-def get_dataloader(args):
-    patches_imgs_train, patches_masks_train = get_data_train(
-        data_path_list = args.train_data_path_list,
-        patch_height = args.train_patch_height,
-        patch_width = args.train_patch_width,
-        N_patches = args.N_patches,
-        inside_FOV = args.inside_FOV #select the patches only inside the FOV  (default == False)
-    )
-    val_ind = random.sample(range(patches_masks_train.shape[0]),int(np.floor(args.val_ratio*patches_masks_train.shape[0])))
-    train_ind =  set(range(patches_masks_train.shape[0])) - set(val_ind)
-    train_ind = list(train_ind)
-
-    train_set = TrainDataset(patches_imgs_train[train_ind,...],patches_masks_train[train_ind,...],mode="train")
-    train_loader = DataLoader(train_set, batch_size=args.batch_size,
-                              shuffle=True, num_workers=6)
-
-    val_set = TrainDataset(patches_imgs_train[val_ind,...],patches_masks_train[val_ind,...],mode="val")
-    val_loader = DataLoader(val_set, batch_size=args.batch_size,
-                            shuffle=False, num_workers=6)
-    # Save some samples of feeding to the neural network
-    N_sample = min(patches_imgs_train.shape[0], 50)
-    save_img(group_images((patches_imgs_train[0:N_sample, :, :, :]*255).astype(np.uint8), 10),
-              join(args.outf, args.save, "sample_input_imgs.png"))
-    save_img(group_images((patches_masks_train[0:N_sample, :, :, :]*255).astype(np.uint8), 10),
-              join(args.outf, args.save,"sample_input_masks.png"))
-    return train_loader,val_loader
-
-# train 
-def train(train_loader,net,criterion,optimizer,device):
-    net.train()
-    train_loss = AverageMeter()
-
-    for batch_idx, (inputs, targets) in tqdm(enumerate(train_loader), total=len(train_loader)):
-        inputs, targets = inputs.to(device), targets.to(device)
-        optimizer.zero_grad()
-
-        outputs = net(inputs)
-        loss = criterion(outputs, targets)
-        loss.backward()
-        optimizer.step()
-
-        train_loss.update(loss.item(), inputs.size(0))
-    log = OrderedDict([('train_loss',train_loss.avg)])
-    return log
-
-# val 
-def val(val_loader,net,criterion,device):
-    net.eval()
-    val_loss = AverageMeter()
-    evaluater = Evaluate()
-    with torch.no_grad():
-        for batch_idx, (inputs, targets) in tqdm(enumerate(val_loader), total=len(val_loader)):
-            inputs, targets = inputs.to(device), targets.to(device)
-            outputs = net(inputs)
-            loss = criterion(outputs, targets)
-            val_loss.update(loss.item(), inputs.size(0))
-
-            outputs = outputs.data.cpu().numpy()
-            targets = targets.data.cpu().numpy()
-            evaluater.add_batch(targets,outputs[:,1])
-    log = OrderedDict([('val_loss', val_loss.avg), 
-                       ('val_acc', evaluater.confusion_matrix()[1]), 
-                       ('val_f1', evaluater.f1_score()),
-                       ('val_auc_roc', evaluater.auc_roc())])
-    return log
 
 def main():
     setpu_seed(2021)
@@ -117,18 +45,15 @@ def main():
 
     # criterion = LossMulti(jaccard_weight=0,class_weights=np.array([0.5,0.5]))
     criterion = CrossEntropyLoss2d() # Initialize loss function
-
-    # create a list of learning rate with epochs
-    # lr_epoch = np.array([50, args.N_epochs])
-    # lr_value = np.array([0.001, 0.0001])
-    # lr_schedule = make_lr_schedule(lr_epoch,lr_value)
-    # lr_scheduler = optim.lr_scheduler.StepLR(optimizer,step_size=10,gamma=0.5)
-    # optimizer = optim.SGD(net.parameters(),lr=lr_schedule[0], momentum=0.9, weight_decay=5e-4, nesterov=True)
     optimizer = optim.Adam(net.parameters(), lr=args.lr)
+    # create a list of learning rate with epochs
+    # lr_schedule = make_lr_schedule(np.array([50, args.N_epochs]),np.array([0.001, 0.0001]))
+    # lr_scheduler = optim.lr_scheduler.StepLR(optimizer,step_size=10,gamma=0.5)
     lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.N_epochs, eta_min=0)
     
-    train_loader, val_loader = get_dataloader(args) # create dataloader
-
+    train_loader, val_loader = get_dataloaderV2(args) # create dataloader
+    # train_loader, val_loader = get_dataloader(args)
+    
     if args.val_on_test: 
         print('\033[0;32m===============Validation on Testset!!!===============\033[0m')
         val_tool = Test(args) 